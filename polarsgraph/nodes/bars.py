5bcb072ecd36392f3951b49309f42142a79c2990
oliver.e <evers@dreamwall.be>
2024-12-02 17:11:30 +0100

    working bar chart

--- polarsgraph/nodes/bars.py
+++ polarsgraph/nodes/bars.py
@@ -1,11 +1,7 @@
-import traceback
-
-import yaml
 import polars as pl
-from PySide6 import QtWidgets, QtGui, QtCore, QtCharts
+from PySide6 import QtWidgets, QtGui, QtCharts
 from PySide6.QtCore import Qt
 
-from polarsgraph.log import logger
 from polarsgraph.nodes import GREEN as DEFAULT_COLOR
 from polarsgraph.graph import DISPLAY_CATEGORY
 from polarsgraph.nodes.base import BaseNode, BaseSettingsWidget, BaseDisplay
@@ -36,6 +32,9 @@ class BarsNode(BaseNode):
         if not self.display_widget:
             return
         self.display_widget.set_table(tables[0])
+
+    def clear(self):
+        pass
 
     @property
     def display_widget(self):
@@ -79,7 +78,7 @@ class BarsDisplay(BaseDisplay):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.chart_view)
 
-    def set_table(self, table):
+    def set_table(self, table: pl.LazyFrame):
         chart = QtCharts.QChart()
         self.bar_series = QtCharts.QHorizontalStackedBarSeries()
         chart.addSeries(self.bar_series)
@@ -87,6 +86,8 @@ class BarsDisplay(BaseDisplay):
         self.chart_view.setChart(chart)
         if table is None:
             return
+        table = table.collect(stream=True)
+        make_chart(self.chart_view, table, self)
 
     def get_pixmap(self):
         pixmap = QtGui.QPixmap(self.chart_view.size())
@@ -102,3 +103,54 @@ class BarsDisplay(BaseDisplay):
 
     def image_to_clipboard(self):
         QtWidgets.QApplication.clipboard().setPixmap(self.get_pixmap())
+
+
+def make_chart(
+        chart_view: QtCharts.QChartView, dataframe: pl.DataFrame, parent):
+    # Create the chart
+    chart = QtCharts.QChart()
+    chart_view.setChart(chart)
+    chart_view.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+
+    # Create the Horizontal Stacked Bar Series
+    series = QtCharts.QHorizontalStackedBarSeries()
+
+    # Add data to the series
+    labels = dataframe.columns[0]
+    value_columns = dataframe.columns[1:]
+
+    for col in value_columns:
+        bar_set = QtCharts.QBarSet(col)
+        bar_set.append(dataframe[col].to_list())
+        series.append(bar_set)
+
+    chart.addSeries(series)
+
+    # Create X-axis for categories (labels)
+    categories = dataframe[labels].to_list()
+    axisY = QtCharts.QBarCategoryAxis()
+    axisY.append(categories)
+    chart.addAxis(axisY, Qt.AlignmentFlag.AlignLeft)
+    series.attachAxis(axisY)
+
+    # Create Y-axis for numeric values
+    axisX = QtCharts.QValueAxis()
+    chart.addAxis(axisX, Qt.AlignmentFlag.AlignBottom)
+    series.attachAxis(axisX)
+
+    # chart.setTitle('Horizontal Stacked Bar Chart')
+    chart.legend().setVisible(True)
+    chart.legend().setAlignment(Qt.AlignmentFlag.AlignBottom)
+
+    # Change colors
+    return
+    palette = parent.palette()
+    background_color = palette.color(QtGui.QPalette.Window)
+    chart.setBackgroundBrush(QtGui.QBrush(background_color))
+    text_color = palette.color(QtGui.QPalette.WindowText)
+    title_font = chart.titleFont()
+    title_font.setWeight(QtGui.QFont.Bold)
+    chart.setTitleBrush(QtGui.QBrush(text_color))
+    for axis in chart.axes():
+        axis.setLabelsBrush(QtGui.QBrush(text_color))
+        axis.setTitleBrush(QtGui.QBrush(text_color))
