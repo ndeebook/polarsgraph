5ad9d86ce3f26b121fa01da18ca3f65634708d15
oliver.e <evers@dreamwall.be>
2025-04-03 11:55:05 +0200

    bars: custom colors

--- polarsgraph/nodes/bars.py
+++ polarsgraph/nodes/bars.py
@@ -17,6 +17,18 @@ COLOR = dict(
     bg_lines=QtGui.QColor('#111111'),
 )
 
+DEFAULT_COLORS = [
+    '#4F77A8',
+    '#76B7B1',
+    '#F28E2C',
+    '#E15659',
+    '#59A14F',
+    '#ECC947',
+    '#B17AA1',
+    '#FE9DA6',
+    '#9B755E',
+]
+
 
 class ATTR:
     NAME = 'name'
@@ -62,19 +74,96 @@ class BarsSettingsWidget(BaseSettingsWidget):
         self.title_edit.editingFinished.connect(
             lambda: self.line_edit_to_settings(self.title_edit, ATTR.TITLE))
 
+        self.colors_table = QtWidgets.QTableWidget(minimumHeight=250)
+        self.colors_table.setColumnCount(2)
+        self.colors_table.horizontalHeader().setSectionResizeMode(
+            QtWidgets.QHeaderView.ResizeMode.Stretch)
+        self.colors_table.setHorizontalHeaderLabels(['Column', ''])
+        self.colors_table.cellChanged.connect(self.set_colors_from_table)
+        self.colors_table.cellClicked.connect(self.edit_color)
+
+        add_button = QtWidgets.QPushButton(
+            'add', clicked=self.add_column)
+        remove_button = QtWidgets.QPushButton(
+            'remove', clicked=self.remove_selected_rows)
+
         # Layout
+        buttons_layout = QtWidgets.QHBoxLayout()
+        buttons_layout.addWidget(add_button)
+        buttons_layout.addWidget(remove_button)
+
         form_layout = QtWidgets.QFormLayout()
         form_layout.addRow(ATTR.NAME.title(), self.name_edit)
         form_layout.addRow(ATTR.TITLE.title(), self.title_edit)
         layout = QtWidgets.QVBoxLayout(self)
         layout.addLayout(form_layout)
+        layout.addWidget(QtWidgets.QLabel('Colors:'))
+        layout.addWidget(self.colors_table)
+        layout.addLayout(buttons_layout)
 
     def set_node(self, node, input_tables):
         self.blockSignals(True)
         self.node = node
         self.name_edit.setText(node[ATTR.NAME])
         self.title_edit.setText(node[ATTR.TITLE] or '')
+        self.populate_color_table()
         self.blockSignals(False)
+
+    def _add_row(self, row_index, column='', color=None):
+        self.colors_table.blockSignals(True)
+        try:
+            color = color or DEFAULT_COLORS[row_index]
+        except IndexError:
+            color = '#111111'
+        column_item = QtWidgets.QTableWidgetItem(column)
+        # column_item.setFlags(Qt.ItemFlag.ItemIsEnabled)
+        self.colors_table.setItem(row_index, 0, column_item)
+
+        column_item2 = QtWidgets.QTableWidgetItem(color)
+        column_item2.setFlags(
+            Qt.ItemFlag.ItemIsEnabled & ~Qt.ItemFlag.ItemIsEditable)
+        column_item2.setBackground(QtGui.QColor(color))
+
+        self.colors_table.setItem(row_index, 1, column_item2)
+        self.colors_table.blockSignals(False)
+
+    def populate_color_table(self):
+        self.colors_table.blockSignals(True)
+        colors = self.node[ATTR.COLORS] or {}
+        self.colors_table.setRowCount(len(colors))
+        for i, (column, color) in enumerate(colors.items()):
+            self._add_row(i, column, color)
+        self.colors_table.blockSignals(False)
+
+    def set_colors_from_table(self):
+        colors = {}
+        for row_index in range(self.colors_table.rowCount()):
+            column = self.colors_table.item(row_index, 0).text()
+            color = self.colors_table.item(row_index, 1).text()
+            colors[column] = color
+        self.node[ATTR.COLORS] = colors
+        self.emit_changed()
+
+    def edit_color(self, row_index, col):
+        if col != 1:
+            return
+        color = QtWidgets.QColorDialog.getColor()
+        if not color.isValid():
+            return
+        self.colors_table.item(row_index, 1).setBackground(color)
+        self.colors_table.item(row_index, 1).setText(color.name())
+        self.set_colors_from_table()
+
+    def add_column(self):
+        count = self.colors_table.rowCount()
+        self.colors_table.setRowCount(count + 1)
+        self._add_row(count)
+
+    def remove_selected_rows(self):
+        for index in self.colors_table.selectedIndexes():
+            column = self.colors_table.item(index.row(), 0).text()
+            self.node[ATTR.COLORS].pop(column, None)
+        self.populate_color_table()
 
 
 class BarsDisplay(BaseDisplay):
@@ -171,8 +260,9 @@ class CustomStackedBarChart(QtWidgets.QWidget):
             painter.drawText(pos_rect, alignment, f'{int(max_value * pos)}')
 
         # Legend
-        colors = get_bars_colors(
-            self.dataframe.columns[1:], self.node[ATTR.COLORS])
+        colors = self.node[ATTR.COLORS] or {}
+        colors = {n: QtGui.QColor(c) for n, c in colors.items()}
+        colors = get_bars_colors(self.dataframe.columns[1:], colors)
         y = rect.bottom() - legend_offset + margin / 2
         width = (rect.width() - 200) / len(colors) - margin
         painter.setPen(Qt.NoPen)
